0b93d1b22f934d652fcabaef89dd6bd2506132c6
Stan Gumeniuk <i@vigo.su>
2016-12-03 15:31:08 +0300

    dimension

--- src/com/gumeniuk/FirstFrame.java
+++ src/com/gumeniuk/FirstFrame.java
@@ -5,5 +5,14 @@ import java.awt.*;
 public class FirstFrame extends JFrame{
     public FirstFrame() throws HeadlessException {
         super(com.gumeniuk.Resources.Resources.string("application.title"));
+        
+        initComponents();
+    }
+
+    private void initComponents() {
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setLocationByPlatform(true);
+        Dimension dimension = new Dimension(300,400);
+        setSize(dimension);
     }
 }
--- src/com/gumeniuk/Main.java
+++ src/com/gumeniuk/Main.java
@@ -5,8 +5,8 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
-        Locale ru = Locale.forLanguageTag("ru");
-        Locale.setDefault(ru);
+//        Locale ru = Locale.forLanguageTag("ru");
+//        Locale.setDefault(ru);
         new FirstFrame().setVisible(true);
     }
 }
