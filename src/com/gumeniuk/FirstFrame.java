7007ac5542ea299ac947d80a51428004fa1645b4
Stan Gumeniuk <i@vigo.su>
2016-12-03 16:26:30 +0300

    add controls

--- src/com/gumeniuk/FirstFrame.java
+++ src/com/gumeniuk/FirstFrame.java
@@ -2,17 +2,38 @@ package com.gumeniuk;
 
 import javax.swing.*;
 import java.awt.*;
-public class FirstFrame extends JFrame{
+
+import com.gumeniuk.Resources.Resources;
+
+public class FirstFrame extends JFrame {
+
+    private final JButton button;
+    private final JTextField textField;
+    private final JLabel label;
+
     public FirstFrame() throws HeadlessException {
-        super(com.gumeniuk.Resources.Resources.string("application.title"));
-        
+        super(Resources.string("application.title"));
+
+        label = new JLabel(Resources.string("label.message"),JLabel.CENTER);
+        button = new JButton(Resources.string("button.push.me"));
+        textField = new JTextField(16);
+
         initComponents();
     }
 
     private void initComponents() {
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setLocationByPlatform(true);
-        Dimension dimension = new Dimension(300,400);
-        setSize(dimension);
+
+        Dimension dimension = new Dimension(300, 400);
+        getContentPane().setPreferredSize(dimension);
+
+
+        add(textField, BorderLayout.NORTH);
+        add(button, BorderLayout.SOUTH);
+
+        add(label, BorderLayout.CENTER);
+
+        pack();
     }
 }
--- src/com/gumeniuk/Main.java
+++ src/com/gumeniuk/Main.java
@@ -1,5 +1,6 @@
 package com.gumeniuk;
 
+import java.awt.*;
 import java.util.Locale;
 
 public class Main {
@@ -7,6 +8,12 @@ public class Main {
     public static void main(String[] args) {
 //        Locale ru = Locale.forLanguageTag("ru");
 //        Locale.setDefault(ru);
-        new FirstFrame().setVisible(true);
+        FirstFrame frame = new FirstFrame();
+
+        Dimension dimension = new Dimension(300, 400);
+
+        frame.setMinimumSize(dimension);
+
+        frame.setVisible(true);
     }
 }
